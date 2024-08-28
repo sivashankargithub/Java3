85c05645ed0779e4657de45db5ebde08c8a3d492
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2024-08-28 10:25:32 +0530

    Static Block and Static Initializers

--- src/pkg1/S2.java
+++ src/pkg1/S2.java
@@ -1,22 +1,19 @@
+//Static Block and Static Initializers
 package pkg1;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class StaticBlock2 {
-	
+public class S2 {
 	private static List<String> list1;
 	static int n1=1;
-	
-    StaticBlock2(){
+    S2(){
     	list1.add("five");
         list1.add("six");
     }
-    
     public void printList(){
         System.out.println(list1);
     }
-    
     static {
     	list1 = new ArrayList<>();
     	list1.add("one");
@@ -25,13 +22,9 @@ public class StaticBlock2 {
     	list1.add("four");
     }
     public static void main(String[] args){
-    	StaticBlock2 sb2 = new StaticBlock2();
+    	S2 sb2 = new S2();
     	sb2.printList();
     	System.out.println(n1);
-        
-        // Calling of the static block
-        // Nothing to do here as it is called
-        // automatically as class is loaded in memory
-        
+    	
     }
 }
