80cb2dc277c7d7d821611495e2bea5692805cc47
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2024-09-02 19:40:10 +0530

    Object Oriented Programming. Understanding of Getters and Setters.

---
+++ src/pkg1/S14.java
@@ -0,0 +1,45 @@
+package pkg1;
+
+public class S14 {
+	
+	private int id;
+	private String name;
+	private int age;
+	private long mobile;
+	
+	public S14() {
+	}
+	
+	public S14(int id, String name, int age, long mobile) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.mobile = mobile;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+}
---
+++ src/pkg1/S15.java
@@ -0,0 +1,17 @@
+package pkg1;
+
+public class S15 {
+
+	public static void main(String[] args) {
+		S14 s14 = new S14();
+		s14.setId(1);
+		s14.setName("Harsha");
+		s14.setAge(19);
+		s14.setMobile(829618);
+		System.out.println(s14.getId());
+		System.out.println(s14.getName());
+		System.out.println(s14.getMobile());
+		System.out.println(s14.getAge());
+	}
+
+}
