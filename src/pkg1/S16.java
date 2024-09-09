f4978f29b2f2264723f0c331bcd5d46641b91e75
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2024-09-09 19:39:39 +0530

    Bank Balance

--- src/pkg1/S16.java
+++ src/pkg1/S16.java
@@ -4,16 +4,9 @@ package pkg1;
 public class S16 {
     public static void main(String[] args) {
         int size1=10;
-        for(int j=1;j<size1;j++){
-            for (int i = 0; i < j; i++) {
-                System.out.print('.');
-            }
-            System.out.println();
-        }
         for (int j = 0; j < size1; j++) {
-            for (int i = j; i < size1; i++) {
+            for (int i = j; i < size1; i++)
                 System.out.print('.');
-            }
             System.out.println();
         }
     }
---
+++ src/pkg1/S17.java
@@ -0,0 +1,33 @@
+package pkg1;
+
+class Bank3 {
+	private double balance;
+	Bank3(double amount){
+		this.balance=amount;
+	}
+	
+	public void addAmount(double amount) {
+		balance = balance+amount;
+		System.out.println("Balance is : "+balance);
+	}
+	public void displayBalance() {
+		System.out.println("Balance is : "+balance);
+	}
+	public void withdrawAmount(double amount) {
+		if(this.balance<0) {
+			System.out.println("Insufficient Funds");
+		}
+		else {
+			balance=balance-amount;
+			System.out.println("Balance is: "+balance);
+		}
+	}
+}
+public class S17{
+	public static void main(String[] args) {
+		Bank3 bank3 = new Bank3(500.0);
+		bank3.displayBalance();
+		bank3.addAmount(200.0);
+		bank3.withdrawAmount(300.0);
+	}
+}
